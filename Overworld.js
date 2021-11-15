fd809fee3cb439e4e340f621d621258ba9044adb
Marcio Souza <marciosouzaprogramando@gmail.com>
2021-11-15 12:37:54 -0300

    Add OverworldMap

--- Overworld.js
+++ Overworld.js
@@ -3,25 +3,36 @@ class Overworld {
         this.element = config.element;
         this.canvas = this.element.querySelector('.game-canvas');
         this.ctx = this.canvas.getContext('2d')
+        this.map = null;
     }
 
 
-    init() {
-        const image = new Image();
-        image.onload = () => {
-            this.ctx.drawImage(image, 0, 0);
+    startGameLoop() {
+        const step = () => {
+
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+            this.map.drawLowerImage(this.ctx);
+
+            Object.values(this.map.gameObjects).forEach(object => {
+                //object.x += 0.01;
+                object.sprite.draw(this.ctx);
+            })
+
+            this.map.drawUpperImage(this.ctx);
+
+
+            requestAnimationFrame(() => {
+                step();
+            })
         }
-        image.src = 'images/maps/ShopLower.png';
-
-        //Placing some characteres
-        const hero = new GameObject({
-            x: 6,
-            y: 8
-        })
+        step();
+    }
 
 
-        setTimeout(() => {
-            hero.sprite.draw(this.ctx)
-        }, 300)
+
+    init() {
+        this.map = new OverworldMap(window.OverworldMaps.DemoRoom)
+        this.startGameLoop();
     }
 }
